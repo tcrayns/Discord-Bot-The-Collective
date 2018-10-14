37080311ff36f13b48a34d34416066b93fd10a11
TC RAyns <tcrayns@gmail.com>
2018-10-14 02:37:02 -0400

    Add DataStorage Interface and InMemoryImplementation
    IDataStorage.cs and InMemoryStorage.cs

---
+++ DiscordBotCore/Storage/IDataStorage.cs
@@ -0,0 +1,9 @@
+﻿namespace DiscordBotCore
+{
+    public interface IDataStorage
+    {
+        void StoreObject(object Obj, string key);
+
+        T RestoreObject<T>(string key);
+    }
+}
---
+++ DiscordBotCore/Storage/Implementations/InMemoryStorage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DiscordBotCore.Storage.Implementations
+{
+    class InMemoryStorage : IDataStorage
+    { 
+        private readonly Dictionary<string, object> _dictionary = new Dictionary<string, object>();
+        private object obj;
+
+        public void StoreObject(object Obj, string key)
+        {
+            if (_dictionary.ContainsKey(key)) return;
+            _dictionary.Add(key, obj);
+        }
+
+        public T RestoreObject<T>(string key)
+        {
+            if (!_dictionary.ContainsKey(key))
+                throw new ArgumentException($"The Provided key '{key}' wasn't found.");
+
+            return (T)_dictionary[key];
+        }
+
+        public void Hello()
+        {
+            Console.WriteLine("Hi!");
+        }
+    }
+}
