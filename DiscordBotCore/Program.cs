b6f0f011332002b6fb9e7e9753e0aef64976866b
TC RAyns <tcrayns@gmail.com>
2018-10-14 00:59:40 -0400

    Update Program.cs
    added internal to class Program
    added private to static void Main(string[] args)
    Changed "Hello World!" to "Hello, Discord!"

--- DiscordBotCore/Program.cs
+++ DiscordBotCore/Program.cs
@@ -2,11 +2,11 @@
 
 namespace DiscordBotCore
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, Discord!");
         }
     }
 }
