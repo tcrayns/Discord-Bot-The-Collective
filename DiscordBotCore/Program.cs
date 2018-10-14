cfb10a5b6e3df7b8755c57294cb3e74c823d1dec
TC RAyns <tcrayns@gmail.com>
2018-10-14 02:37:42 -0400

    Add Unity Container
    Implemented basic Unity container and redistered IdataStorage.

--- DiscordBotCore/Program.cs
+++ DiscordBotCore/Program.cs
@@ -4,8 +4,9 @@ namespace DiscordBotCore
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
+            Unity.RegisterTypes(); 
             Console.WriteLine("Hello, Discord!");
         }
     }
